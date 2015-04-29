96b9bb88dcef584488d0c2b1799644937af64b5d
Konrad Grochowski <hcorg@minions.org.pl>
2015-04-29 11:13:57 +0200

    dropdown-list moved to melpa and connected with yasnippet

--- _emacs.d/custom-yasnippet.el
+++ _emacs.d/custom-yasnippet.el
@@ -1,2 +1,7 @@
 (require 'yasnippet)
 (yas/initialize)
+
+(require 'dropdown-list)
+(setq yas-prompt-functions '(yas-dropdown-prompt
+                             yas-ido-prompt
+                             yas-completing-prompt))
--- install_packages.el
+++ install_packages.el
@@ -12,6 +12,7 @@
   '(magit
     thrift
     color-theme
+    dropdown-list
     yasnippet
     auto-complete
     auto-complete-clang
